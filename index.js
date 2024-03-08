c4b1d18d307a8e993d475b46a2241776a77dee92
Mona Paul <ahyammonapaul5@gmail.com>
2024-03-08 16:59:13 +0100

    Solana Bot

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const { Connection,Keypair, VersionedTransaction,  PublicKey } = require("@solana/web3.js");
-const fetch = require("cross-fetch");
+//const fetch = require("cross-fetch");
+const fetch = require("node-fetch");
+const { Jupiter, RouteInfo, TOKEN_LIST_URL, SwapResult } = require('@jup-ag/core');
+const JSBI = require('jsbi');
+const swap = require("./swapAmm.js");
 const { Wallet } = require('@project-serum/anchor');
+const { Token, TOKEN_PROGRAM_ID, getOrCreateAssociatedTokenAccount } = require('@solana/spl-token');
+
 const bs58 = require('bs58');
-const { Token, AccountLayout, u64 } = require('@solana/spl-token');
-const { TokenListProvider } = require("@solana/spl-token-registry");
-const {TokenAccount,SPL_ACCOUNT_LAYOUT,LIQUIDITY_STATE_LAYOUT_V4,} = require("@raydium-io/raydium-sdk");
+//const { RaydiumSwap } = require("./swap");
+const { TokenSwap } = require("@solana/spl-token-swap");
+//const test = require("./src/decode_instruction");
+const { LIQUIDITY_STATE_LAYOUT_V4, Liquidity} = require("@raydium-io/raydium-sdk");
 const BN = require('bn.js');
 
+
 const TelegramBot = require("node-telegram-bot-api");
+const { createJupiterApiClient } = require('@jup-ag/api');
+
+
 
 const e = require("express");
+
 TELEGRAM_BOT_TOKEN = "6534890049:AAEa-J3-GFlu6gY5E7p5gVF538NhWERdqw4"
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling : true});
@@ -26,7 +38,8 @@ let bought = false;
 const SOLANA = "So11111111111111111111111111111111111111112"
 const RAYDIUM_PUBLIC_KEY = "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8";
 const Raydium_Authority_PUBLIC_KEY = "5Q544fKrFoe6tsEbD7S8EmxGTJYAKtTVhAW5Q5pge4j1";
-
+const RAY_SOL_LP_V4_POOL_KEY = '89ZKE4aoyfLBe2RuV6jM3JGNhaV18Nxh8eNtjRcndBip';
+const RAYDIUM_LIQUIDITY_JSON = 'https://api.raydium.io/v2/sdk/liquidity/mainnet.json';
 const privateKey = new Uint8Array([
   190, 129, 89, 173, 55, 130, 139, 23, 88, 207, 184, 103, 72, 95, 227, 117, 84, 176, 28, 102, 157, 220, 115, 141, 112, 27, 39, 106, 235, 152, 176, 129, 204, 127, 84, 35, 30, 174, 194, 82, 218, 197, 41, 29, 40, 127, 141, 231, 136, 82, 169, 81, 35, 98, 54, 198, 168, 30, 48, 46, 231, 166, 154, 193
 ]);
@@ -124,7 +137,7 @@ async function fetchRaydiumAccounts(txId) {
         { "Token": "A", "Account Public Key": tokenAAccount.toBase58() },
         { "Token": "B", "Account Public Key": tokenBAccount.toBase58() }
     ];
-     const [initialLP, vault,startTime] = await getPoolInfo(lpAccount);
+     const [initialLP, vault,startTime, decimal] = await getPoolInfo(lpAccount);
    
     if(
       initialLP % 1 !== 0
@@ -181,8 +194,8 @@ async function fetchRaydiumAccounts(txId) {
        startTime: ${startTime}
        Whole
      `)
-     orderBuys(msUntilTarget,tokenAAccount);
-     getChanges(vaultAddress,pairAddress);
+    await  orderBuys(msUntilTarget,tokenAAccount,lpAccount,decimal);
+     getChanges(vaultAddress,tokenAAccount,pairAddress,decimal);
     }else{
       trade = true
       main(connection, raydium).catch(console.error);
@@ -198,6 +211,7 @@ async function fetchRaydiumAccounts(txId) {
 async function getPoolInfo(lpToken){
     let mainCheck;
     let mainAddress;
+    let decimal;
 
     const pair = new PublicKey(lpToken);
     
@@ -214,16 +228,18 @@ async function getPoolInfo(lpToken){
             poolState.quoteVault
          )
          mainAddress = poolState.quoteVault;
+         decimal = poolState.quoteDecimal
       }else{
         mainCheck = await solConnection.getTokenAccountBalance(
             poolState.baseVault
         )
         mainAddress = poolState.baseVault;
+        decimal = poolState.baseDecimal;
       }
     
       const denominator = new BN(10).pow(poolState.baseDecimal);
 
-    return [mainCheck.value.uiAmount, mainAddress,startDate];
+    return [mainCheck.value.uiAmount, mainAddress,startDate, decimal];
 }
 
 // async function getTime(lpToken){
@@ -239,7 +255,69 @@ async function getPoolInfo(lpToken){
 //    return startDate;
 // }
 
-async function getChanges(address, lp){
+// const swap = async () => {
+//   const INPUT_MINT_ADDRESS = SOLANA
+//   const OUTPUT_MINT_ADDRESS = "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB"
+//   console.log("Initializing Swap");
+//   // A helper function to help us find which output pair is possible
+  
+  
+//   // Create a new Jupiter instance
+//   const jupiter = await Jupiter.load({
+//     connection,
+//     user: wallet, // or public key
+//     cluster: 'mainnet-beta',
+//     wrapUnwrapSOL: true,
+   
+//     routeCacheDuration: 60000, // 60 seconds
+//   });
+
+//   console.log(`Jupiter instance: ${jupiter}`);
+//      // Get routeMap, which maps each tokenMint and their respective tokenMints that are swappable
+//   const routeMap = jupiter.getRouteMap();
+  
+//   // If you know which input/output pair you want
+//   const inputToken = tokens.find((t) => t.address == INPUT_MINT_ADDRESS);
+//   const outputToken = tokens.find((t) => t.address == OUTPUT_MINT_ADDRESS);
+//   // Alternatively, check step 4
+//   const routes = await jupiter.computeRoutes({
+//     inputMint: new PublicKey(inputToken.address),
+//     outputMint: new PublicKey(outputToken.address),
+//     amount: JSBI.BigInt(100000), // 1000000 => 1 USDC if inputToken.address is USDC mint.
+//     slippageBps  // 1 bps = 0.01%.
+//     // forceFetch (optional) => to force fetching routes and not use the cache.
+//     // intermediateTokens => if provided will only find routes that use the intermediate tokens.
+//     // feeBps => the extra fee in BPS you want to charge on top of this swap.
+//     // onlyDirectRoutes =>  Only show single hop routes.
+//     // swapMode => "ExactIn" | "ExactOut" Defaults to "ExactIn"  "ExactOut" is to support use cases like payments when you want an exact output amount.
+//     // enforceSingleTx =>  Only show routes where only one single transaction is used to perform the Jupiter swap.
+//   });
+
+//   const inputAmount = 1; // UI input
+//   const inputTokenInfo = tokens.find(item => item.address === SOLANA); // Token info
+//   const amount = inputAmount * (10 ** inputTokenInfo.decimals); // Amount to send to Jupiter
+//   // Routes are sorted based on outputAmount, so ideally the first route is the best.
+//   bestRoute = routes.routesInfos[0]
+//   const { execute } = await jupiter.exchange({
+//     routeInfo: bestRoute
+//   });
+//    console.log("Almost Executing swap " + bestRoute);
+//   // Execute swap
+//   const swapResult = await execute();
+
+//   if (swapResult.error) {
+//     console.log(swapResult.error);
+//   } else {
+//     console.log(`https://explorer.solana.com/tx/${swapResult.txid}`);
+//     console.log(`inputAddress=${swapResult.inputAddress.toString()} outputAddress=${swapResult.outputAddress.toString()}`);
+//     console.log(`inputAmount=${swapResult.inputAmount} outputAmount=${swapResult.outputAmount}`);
+//   }
+
+//   // ...
+
+// }
+
+async function getChanges(address,token, lp, decimal){
     let addr = new PublicKey(address);
    
     const subscriptionID = transConnection.onAccountChange(
@@ -260,7 +338,8 @@ async function getChanges(address, lp){
       }else{
       const solBal = updatedAccountInfo.lamports/1000000000
       let profit = Number(solBal).toFixed(2) / Number(initialBalance).toFixed(2); 
-     if(profit > target){ 
+     if(profit > target){
+       await orderSell(token,lp,decimal) 
        bot.sendMessage(msgId,` 
        Liquidity Pair: ${lp} 
        Target hit  ${profit}
@@ -284,74 +363,30 @@ async function getChanges(address, lp){
     }
     }
   )
-    }
-async function orderBuys(msUntilTarget,outputMint ){
-// Swapping SOL to USDC with input 0.1 SOL and 0.5% slippage
- const amount = 100000;
- const quoteResponse = await (
-   await fetch(`https://quote-api.jup.ag/v6/quote?inputMint=So11111111111111111111111111111111111111112\
-  &outputMint=${outputMint}\
-  &amount=${amount}\
-  &slippageBps=50`
-   )
-  ).json();
-  console.log({ quoteResponse })
-
-const { swapTransaction } = await (
-  await fetch('https://quote-api.jup.ag/v6/swap', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      // quoteResponse from /quote api
-      quoteResponse,
-      // user public key to be used for the swap
-      userPublicKey: wallet.publicKey.toString(),
-      // auto wrap and unwrap SOL. default is true
-      wrapAndUnwrapSol: true,
-      // feeAccount is optional. Use if you want to charge a fee.  feeBps must have been passed in /quote API.
-      // feeAccount: "fee_account_public_key"
-      prioritizationFeeLamports: '1000000' // or custom lamports: 1000
-
-    })
-  })
-).json();
-
-
-setTimeout( async() => {
-  // Connect to the Solana devnet cluster
-  // const connection = new Connection('https://api.devnet.solana.com');
-   // deserialize the transaction
-  try {
-    
-
-  const swapTransactionBuf = Buffer.from(swapTransaction, 'base64');
-  var transaction = VersionedTransaction.deserialize(swapTransactionBuf);
-  console.log(transaction);
-
-  // sign the transaction
-   transaction.sign([wallet.payer]);
-
-   // Execute the transaction
-   const rawTransaction = transaction.serialize()
-   const txid = await connection.sendRawTransaction(rawTransaction, {
-     skipPreflight: true,
-     maxRetries: 2
-    });
-   await connection.confirmTransaction(txid);
-   console.log(`https://solscan.io/tx/${txid}`);
-
-  console.log("Buy here");
-  } catch (error) {
-    console.log(error);  
-  }
-
-  }, msUntilTarget - 4000);
 }
+
+async function orderBuys(msUntilTarget,token,pool, decimal) {
+  const amount = 10000
+  setTimeout( async() => {
+  await swap.Buy(token,pool,amount,decimal);
+  },msUntilTarget - 4000);
+}
+async function orderSell(token,pool,decimal) {
+  const walletAddress = new PublicKey("EmGnGTRQdKxV3f328W71VUedVvAGVZZASZsxCKNSF5L4");
+  const tokenAdd = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
+  const tokenAddress = await getOrCreateAssociatedTokenAccount(mainConnection,wallet, tokenAdd  ,wallet.publicKey )// RAY
+ // const getTokenAccountBalance = await tokenConnection.getTokenAccountsByOwner(walletAddress,tokenAddress);
+  const balance = Number(tokenAddress.amount);
+   
+  await swap.Sell(token,pool,balance,decimal);
+}
+
  function generateExplorerUrl(txId) {
     return `https://solscan.io/tx/${txId}`;
  }
  
-main(connection, raydium).catch(console.error);
+//main(connection, raydium).catch(console.error);
 //getPoolInfo('7ipQJShoKhER2gR8feGJ4HDnGb5xTUbHMCdVRomsCMbX')
+//swap.howToUse();
+//test.decode();
+orderSell("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v","58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2",6);
---
+++ swapAmm.js
@@ -0,0 +1,310 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+exports.__esModule = true;
+var assert = require("assert");
+var anchor_1 = require("@project-serum/anchor");
+var raydium_sdk_1 = require("@raydium-io/raydium-sdk");
+var web3_js_1 = require("@solana/web3.js");
+var SESSION_HASH = 'QNDEMO' + Math.ceil(Math.random() * 1e9); // Random unique identifier for your session
+var makeTxVersion = raydium_sdk_1.TxVersion.V0; // LEGACY
+var connection = new web3_js_1.Connection("https://solana-mainnet.core.chainstack.com/2b9789b958c420270f3e09b3fac2d299", {
+    wsEndpoint: "wss://solana-mainnet.core.chainstack.com/ws/2b9789b958c420270f3e09b3fac2d299",
+    httpHeaders: { "x-session-hash": SESSION_HASH },
+    commitment: 'confirmed'
+});
+var mainConnection = new web3_js_1.Connection("https://solana-mainnet.g.alchemy.com/v2/ivbpOnYRAvSjoLJEpPNP910PYIcrtNrw", {
+    wsEndpoint: "wss://solana-mainnet.g.alchemy.com/v2/ivbpOnYRAvSjoLJEpPNP910PYIcrtNrw",
+    httpHeaders: { "x-session-hash": SESSION_HASH },
+    commitment: 'confirmed'
+});
+var DEFAULT_TOKEN = {
+    'SOL': new raydium_sdk_1.Currency(9, 'USDC', 'USDC'),
+    'WSOL': new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey('So11111111111111111111111111111111111111112'), 9, 'WSOL', 'WSOL'),
+    'USDC': new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'), 6, 'USDC', 'USDC'),
+    'RAY': new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey('34K23tYU71NbNypSA2TZ9M3xA8kF1pCBU7r5LwhAmEaK'), 6),
+    'RAY_USDC-LP': new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey('FGYXP4vBkMEtKhxrmEBcWN8VNmXX8qNgEJpENKDETZ4Y'), 6, 'RAY-USDC', 'RAY-USDC')
+};
+var privateKey = new Uint8Array([
+    190, 129, 89, 173, 55, 130, 139, 23, 88, 207, 184, 103, 72, 95, 227, 117, 84, 176, 28, 102, 157, 220, 115, 141, 112, 27, 39, 106, 235, 152, 176, 129, 204, 127, 84, 35, 30, 174, 194, 82, 218, 197, 41, 29, 40, 127, 141, 231, 136, 82, 169, 81, 35, 98, 54, 198, 168, 30, 48, 46, 231, 166, 154, 193
+]);
+var wallet = new anchor_1.Wallet(web3_js_1.Keypair.fromSecretKey(privateKey));
+var addLookupTableInfo = raydium_sdk_1.LOOKUP_TABLE_CACHE;
+function formatAmmKeysById(id) {
+    return __awaiter(this, void 0, void 0, function () {
+        var account, info, marketId, marketAccount, marketInfo, lpMint, lpMintAccount, lpMintInfo;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, connection.getAccountInfo(new web3_js_1.PublicKey(id))];
+                case 1:
+                    account = _a.sent();
+                    if (account === null)
+                        throw Error(' get id info error ');
+                    info = raydium_sdk_1.LIQUIDITY_STATE_LAYOUT_V4.decode(account.data);
+                    marketId = info.marketId;
+                    return [4 /*yield*/, connection.getAccountInfo(marketId)];
+                case 2:
+                    marketAccount = _a.sent();
+                    if (marketAccount === null)
+                        throw Error(' get market info error');
+                    marketInfo = raydium_sdk_1.MARKET_STATE_LAYOUT_V3.decode(marketAccount.data);
+                    lpMint = info.lpMint;
+                    return [4 /*yield*/, connection.getAccountInfo(lpMint)];
+                case 3:
+                    lpMintAccount = _a.sent();
+                    if (lpMintAccount === null)
+                        throw Error(' get lp mint info error');
+                    lpMintInfo = raydium_sdk_1.SPL_MINT_LAYOUT.decode(lpMintAccount.data);
+                    return [2 /*return*/, {
+                            id: id,
+                            baseMint: info.baseMint.toString(),
+                            quoteMint: info.quoteMint.toString(),
+                            lpMint: info.lpMint.toString(),
+                            baseDecimals: info.baseDecimal.toNumber(),
+                            quoteDecimals: info.quoteDecimal.toNumber(),
+                            lpDecimals: lpMintInfo.decimals,
+                            version: 4,
+                            programId: account.owner.toString(),
+                            authority: raydium_sdk_1.Liquidity.getAssociatedAuthority({ programId: account.owner }).publicKey.toString(),
+                            openOrders: info.openOrders.toString(),
+                            targetOrders: info.targetOrders.toString(),
+                            baseVault: info.baseVault.toString(),
+                            quoteVault: info.quoteVault.toString(),
+                            withdrawQueue: info.withdrawQueue.toString(),
+                            lpVault: info.lpVault.toString(),
+                            marketVersion: 3,
+                            marketProgramId: info.marketProgramId.toString(),
+                            marketId: info.marketId.toString(),
+                            marketAuthority: raydium_sdk_1.Market.getAssociatedAuthority({ programId: info.marketProgramId, marketId: info.marketId }).publicKey.toString(),
+                            marketBaseVault: marketInfo.baseVault.toString(),
+                            marketQuoteVault: marketInfo.quoteVault.toString(),
+                            marketBids: marketInfo.bids.toString(),
+                            marketAsks: marketInfo.asks.toString(),
+                            marketEventQueue: marketInfo.eventQueue.toString(),
+                            lookupTableAccount: web3_js_1.PublicKey["default"].toString()
+                        }];
+            }
+        });
+    });
+}
+function sendTx(connection, payer, txs, options) {
+    return __awaiter(this, void 0, void 0, function () {
+        var txids, _i, txs_1, iTx, _a, _b, _c, _d;
+        return __generator(this, function (_e) {
+            switch (_e.label) {
+                case 0:
+                    txids = [];
+                    _i = 0, txs_1 = txs;
+                    _e.label = 1;
+                case 1:
+                    if (!(_i < txs_1.length)) return [3 /*break*/, 6];
+                    iTx = txs_1[_i];
+                    if (!(iTx instanceof web3_js_1.VersionedTransaction)) return [3 /*break*/, 3];
+                    iTx.sign([payer]);
+                    _b = (_a = txids).push;
+                    return [4 /*yield*/, connection.sendTransaction(iTx, options)];
+                case 2:
+                    _b.apply(_a, [_e.sent()]);
+                    return [3 /*break*/, 5];
+                case 3:
+                    _d = (_c = txids).push;
+                    return [4 /*yield*/, connection.sendTransaction(iTx, [payer], options)];
+                case 4:
+                    _d.apply(_c, [_e.sent()]);
+                    _e.label = 5;
+                case 5:
+                    _i++;
+                    return [3 /*break*/, 1];
+                case 6: return [2 /*return*/, txids];
+            }
+        });
+    });
+}
+function getWalletTokenAccount(connection, wallet) {
+    return __awaiter(this, void 0, void 0, function () {
+        var walletTokenAccount;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, connection.getTokenAccountsByOwner(wallet, {
+                        programId: raydium_sdk_1.TOKEN_PROGRAM_ID
+                    })];
+                case 1:
+                    walletTokenAccount = _a.sent();
+                    return [2 /*return*/, walletTokenAccount.value.map(function (i) { return ({
+                            pubkey: i.pubkey,
+                            programId: i.account.owner,
+                            accountInfo: raydium_sdk_1.SPL_ACCOUNT_LAYOUT.decode(i.account.data)
+                        }); })];
+            }
+        });
+    });
+}
+function buildAndSendTx(innerSimpleV0Transaction, options) {
+    return __awaiter(this, void 0, void 0, function () {
+        var willSendTx;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, (0, raydium_sdk_1.buildSimpleTransaction)({
+                        connection: connection,
+                        makeTxVersion: makeTxVersion,
+                        payer: wallet.publicKey,
+                        innerTransactions: innerSimpleV0Transaction,
+                        addLookupTableInfo: addLookupTableInfo
+                    })];
+                case 1:
+                    willSendTx = _a.sent();
+                    return [4 /*yield*/, sendTx(connection, wallet.payer, willSendTx, options)];
+                case 2: return [2 /*return*/, _a.sent()];
+            }
+        });
+    });
+}
+function swapOnlyAmm(input) {
+    return __awaiter(this, void 0, void 0, function () {
+        var targetPoolInfo, poolKeys, _a, amountOut, minAmountOut, _b, _c, innerTransactions;
+        var _d, _e;
+        return __generator(this, function (_f) {
+            switch (_f.label) {
+                case 0: return [4 /*yield*/, formatAmmKeysById(input.targetPool)];
+                case 1:
+                    targetPoolInfo = _f.sent();
+                    assert(targetPoolInfo, 'cannot find the target pool');
+                    poolKeys = (0, raydium_sdk_1.jsonInfo2PoolKeys)(targetPoolInfo);
+                    _c = (_b = raydium_sdk_1.Liquidity).computeAmountOut;
+                    _d = {
+                        poolKeys: poolKeys
+                    };
+                    return [4 /*yield*/, raydium_sdk_1.Liquidity.fetchInfo({ connection: connection, poolKeys: poolKeys })];
+                case 2:
+                    _a = _c.apply(_b, [(_d.poolInfo = _f.sent(),
+                            _d.amountIn = input.inputTokenAmount,
+                            _d.currencyOut = input.outputToken,
+                            _d.slippage = input.slippage,
+                            _d)]), amountOut = _a.amountOut, minAmountOut = _a.minAmountOut;
+                    return [4 /*yield*/, raydium_sdk_1.Liquidity.makeSwapInstructionSimple({
+                            connection: connection,
+                            poolKeys: poolKeys,
+                            userKeys: {
+                                tokenAccounts: input.walletTokenAccounts,
+                                owner: input.wallet.publicKey
+                            },
+                            amountIn: input.inputTokenAmount,
+                            amountOut: minAmountOut,
+                            fixedSide: 'in',
+                            makeTxVersion: makeTxVersion
+                        })];
+                case 3:
+                    innerTransactions = (_f.sent()).innerTransactions;
+                    console.log('amountOut:', amountOut.toFixed(), '  minAmountOut: ', minAmountOut.toFixed());
+                    _e = {};
+                    return [4 /*yield*/, buildAndSendTx(innerTransactions)];
+                case 4: return [2 /*return*/, (_e.txids = _f.sent(), _e)];
+            }
+        });
+    });
+}
+async function Buy(token, Pool, amount, decimal) {
+    return __awaiter(this, void 0, void 0, function () {
+        var inputToken, outputToken, targetPool, inputTokenAmount, slippage, walletTokenAccounts;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    inputToken = DEFAULT_TOKEN.WSOL // USDC
+                    ;
+                    outputToken = new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey(token), decimal) // RAY
+                    ;
+                    targetPool = Pool // USDC-RAY pool
+                    ;
+                    inputTokenAmount = new raydium_sdk_1.TokenAmount(inputToken, amount);
+                    slippage = new raydium_sdk_1.Percent(1, 100);
+                    return [4 /*yield*/, getWalletTokenAccount(connection, wallet.publicKey)];
+                case 1:
+                    walletTokenAccounts = _a.sent();
+                    swapOnlyAmm({
+                        outputToken: outputToken,
+                        targetPool: targetPool,
+                        inputTokenAmount: inputTokenAmount,
+                        slippage: slippage,
+                        walletTokenAccounts: walletTokenAccounts,
+                        wallet: wallet.payer
+                    }).then(function (_a) {
+                        var txids = _a.txids;
+                        /** continue with txids */
+                        console.log('txids', txids);
+                    });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+async function Sell(token, Pool, amount, decimal) {
+    return __awaiter(this, void 0, void 0, function () {
+        var inputToken, outputToken, targetPool, inputTokenAmount, slippage, walletTokenAccounts;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    inputToken = new raydium_sdk_1.Token(raydium_sdk_1.TOKEN_PROGRAM_ID, new web3_js_1.PublicKey(token), decimal) // USDC
+                    ;
+                    outputToken = DEFAULT_TOKEN.WSOL // RAY
+                    ;
+                    targetPool = Pool // USDC-RAY pool
+                    ;
+                    inputTokenAmount = new raydium_sdk_1.TokenAmount(inputToken, amount);
+                    slippage = new raydium_sdk_1.Percent(1, 100);
+                    return [4 /*yield*/, getWalletTokenAccount(connection, wallet.publicKey)];
+                case 1:
+                    walletTokenAccounts = _a.sent();
+                    swapOnlyAmm({
+                        outputToken: outputToken,
+                        targetPool: targetPool,
+                        inputTokenAmount: inputTokenAmount,
+                        slippage: slippage,
+                        walletTokenAccounts: walletTokenAccounts,
+                        wallet: wallet.payer
+                    }).then(function (_a) {
+                        var txids = _a.txids;
+                        /** continue with txids */
+                        console.log('txids', txids);
+                    });
+                    return [2 /*return*/];
+            }
+        });
+    });
+}
+console.log("why");
+
+module.exports = {Buy,Sell};
