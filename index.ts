b3a34de71027b37b0438e7a086290c073328ec6a
Mona Paul <ahyammonapaul5@gmail.com>
2024-04-16 14:05:29 +0100

    Trading Bot v1.1.0

--- index.ts
+++ index.ts
@@ -275,7 +275,9 @@ bot.on('text', async(ctx) => {
       const targetTime : any = new Date(tokenInfo.startTime.getFullYear(), tokenInfo.startTime.getMonth(),tokenInfo.startTime.getDate(), tokenInfo.startTime.getHours(), tokenInfo.startTime.getMinutes(), tokenInfo.startTime.getSeconds(),tokenInfo.startTime.getMilliseconds());
       const timeDiff : any = targetTime - now;
       const msUntilTarget = timeDiff > 0 ? timeDiff : 86400000 - Math.abs(timeDiff);
+      if(now >= targetTime){
       await orderBuys(tokenInfo.token.toString(),tokenInfo.lp.toString(),Number(tokenInfo.decimal))
+      }
        const MAX_RETRIES = 10000;
        const BASE_DELAY = 1000;
        let retries = 0;
--- jito_bundle/send_bundle.ts
+++ jito_bundle/send_bundle.ts
@@ -6,7 +6,7 @@ import { Wallet } from '@project-serum/anchor';
 import { Connection, Keypair } from '@solana/web3.js';
 
  //export const blockEngineUrl = 'tokyo.mainnet.block-engine.jito.wtf';
-export const blockEngineUrl = 'frankfurt.mainnet.block-engine.jito.wtf';
+export const blockEngineUrl = 'amsterdam.mainnet.block-engine.jito.wtf';
  const privateKey = new Uint8Array([
   228,110,17,108,138,39,103,38,106,160,16,109,222,208,45,94,230,140,168,148,7,166,98,119,236,96,146,0,195,58,217,250,24,236,168,240,15,121,103,31,131,5,134,250,32,85,157,61,17,183,118,63,13,234,77,232,151,30,62,120,119,73,185,174
 ]);
